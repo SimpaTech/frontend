577c498d1a0734649d709bb159b3ecb3be9f27b5
Jean Faria <111452998+jeejinf@users.noreply.github.com>
2024-04-02 09:38:57 -0300

    Adicionando a rota da Estacao e importando o seu CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import EstacaoCadastro from './components/EstacaoCadastro';
+import Estacao from './components/Estacao';
 
 const App = () => {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/estacoes/cadastro" element={<EstacaoCadastro />} />
+          <Route path="/estacoes" element={<Estacao />} />
         </Routes>
       </div>
     </Router>
