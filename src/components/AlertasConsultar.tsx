cdab701ae64392b09f1ea7e8eefece23a8563a8a
Jean Faria <111452998+jeejinf@users.noreply.github.com>
2024-04-07 23:44:40 -0300

    Adicionado o componente AlertasConsultar e a sua estilização

---
+++ src/components/AlertasConsultar.tsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react"
+import "../styles/AlertasConsultar.css"
+import { Container } from "react-bootstrap"
+
+type Props = {}
+
+type State = {}
+
+export default class AlertasConsultar extends Component<Props, State> {
+  state = {}
+
+  render() {
+    return (
+      <Container>
+        <h1>AlertasConsultar</h1>
+      </Container>
+    )
+  }
+}
