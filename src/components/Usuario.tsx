51b8341050dabc9a7024fc62775ad423c61f0577
Jean Faria <111452998+jeejinf@users.noreply.github.com>
2024-04-10 12:45:03 -0300

    Editando o componente de Usuário para a incluir a tela de Edição

--- src/components/Usuario.tsx
+++ src/components/Usuario.tsx
@@ -1,65 +1,74 @@
-import React, { Component } from "react"
-import Sidebar from "./Sidebar"
-import "../styles/Usuario.css"
-import { Container } from "react-bootstrap"
-import UsuarioCadastro from "./UsuarioCadastro"
-import UsuarioConsultar from "./UsuarioConsultar"
-import Navbar from "./Navbar"
-import Header from "./Header"
+// Usuario.tsx
+import React, { Component } from "react";
+import Sidebar from "./Sidebar";
+import "../styles/Usuario.css";
+import { Container } from "react-bootstrap";
+import UsuarioCadastro from "./UsuarioCadastro";
+import UsuarioConsultar from "./UsuarioConsultar";
+import Navbar from "./Navbar";
+import Header from "./Header";
 import InputMask from "react-input-mask";
+import UsuarioEditar from "./UsuarioEditar";
 
-type Props = {}
+type Props = {};
 
 type State = {
-  currentPage: string
-  hasDashboard: boolean
-}
+  currentPage: string;
+  hasDashboard: boolean;
+  editUserId: number;
+};
 
 export default class Usuario extends Component<Props, State> {
   state: State = {
-    currentPage: "Cadastro", // Definindo a página inicial como "Dashboard"
+    currentPage: "Cadastro", // Definindo a página inicial como "Cadastro"
     hasDashboard: false,
-  }
+    editUserId: 1,
+  };
 
   // Método para alterar a página atual
   changePage = (page: string) => {
-    this.setState({ currentPage: page })
-  }
+    this.setState({ currentPage: page });
+  };
+
+  // Função para lidar com o clique no botão de edição
+  handleEditClick = (userId: number) => {
+    // Atualize o estado com o ID do usuário para edição
+    this.setState({ editUserId: userId });
+    // Navegue para a página de edição
+    this.changePage("Editar");
+  };
 
   render() {
-    // const { username } = useContext(UserContext); // Usar o useContext quando estiver disponível
-    const username = "Felipe AG" // Por enquanto, usarei Felipe AG mockado no Header
+    const username = "Felipe AG"; // Mockando o nome do usuário
 
-    let currentPageContent // Variável para armazenar o conteúdo da página atual
+    let currentPageContent;
 
     // Renderizar o conteúdo da página com base na página atual
     switch (this.state.currentPage) {
       case "Cadastro":
-        currentPageContent = <UsuarioCadastro />
-        break
+        currentPageContent = <UsuarioCadastro />;
+        break;
       case "Consultar":
-        currentPageContent = <UsuarioConsultar />
-        break
+        currentPageContent = <UsuarioConsultar onEditClick={this.handleEditClick} />;
+        break;
+      case "Editar":
+        currentPageContent = <UsuarioEditar userId={this.state.editUserId} onEditClick={() => this.changePage("Consultar")} />;
+        break;
+      default:
+        currentPageContent = null;
     }
 
     return (
       <div className="d-flex" id="wrapper">
-        {/* Sidebar */}
         <Sidebar />
-
         <Container className="background-content p-0">
-          {/* Header */}
           <Header title="Controle de Usuários" username={username} />
-
           <Container>
-            {/* Navbar */}
-            <Navbar changePage={this.changePage} hasDashboard={this.state.hasDashboard} /> {/* Passando a função changePage para a Navbar */}
-
-            {/* Page Content */}
-            {currentPageContent} {/* Renderizando o conteúdo da página atual */}
+            <Navbar changePage={this.changePage} hasDashboard={this.state.hasDashboard} />
+            {currentPageContent}
           </Container>
         </Container>
       </div>
-    )
+    );
   }
 }
