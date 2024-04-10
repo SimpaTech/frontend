68459dad87affe82c7b15b08e9b471f74dc71356
Gabriela Barbosa <gabrielasbarbosa3@gmail.com>
2024-04-10 08:41:17 -0300

    Alterando estrutura do código para manter o padrão com as outras listagens

--- src/components/AlertasConsultar.tsx
+++ src/components/AlertasConsultar.tsx
@@ -1,63 +1,65 @@
-import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
 import { Container, Table, Button } from "react-bootstrap";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { listarAlertas } from "../services/apiService"; 
 
-interface Alerta {
+type Alerta = {
   id: number;
   Nome_Tipo_Alerta: string;
   Valor: number;
   Operador_Condicional: string;
 }
 
-const AlertasListagem: React.FC = () => {
-  const [alertas, setAlertas] = useState<Alerta[]>([]);
-
-  useEffect(() => {
-    async function fetchAlertas() {
-      try {
-        const response = await listarAlertas();
-        setAlertas(response.data);
-      } catch (error) {
-        console.error("Erro ao buscar alertas:", error);
-      }
-    }
-
-    fetchAlertas();
-  }, []);
-
-  return (
-    <Container>
-      <Table striped bordered hover style={{ marginTop: '2%' }} className="text-center">
-        <thead>
-          <tr>
-            <th>Nome</th>
-            <th>Valor</th>
-            <th>Operador Condicional</th>
-            <th>Ações</th>
-          </tr>
-        </thead>
-        <tbody>
-          {alertas.map(alerta => (
-            <tr key={alerta.id}>
-              <td>{alerta.Nome_Tipo_Alerta}</td>
-              <td>{alerta.Valor}</td>
-              <td>{alerta.Operador_Condicional}</td>
-              <td>
-                <Button variant="primary" className="mr-2">
-                  <FontAwesomeIcon icon={faEdit} />
-                </Button>
-                <Button variant="danger">
-                  <FontAwesomeIcon icon={faTrash} />
-                </Button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
-    </Container>
-  );
+type State = {
+  alertas: Alerta[];
 }
 
-export default AlertasListagem;
+export default class AlertasConsultar extends Component<{}, State> {
+  state: State = {
+    alertas: []
+  };
+
+  async componentDidMount() {
+    try {
+      const response = await listarAlertas();
+      this.setState({ alertas: response.data });
+    } catch (error) {
+      console.error("Erro ao buscar alertas:", error);
+    }
+  }
+
+  render() {
+    return (
+      <Container>
+        <Table striped bordered hover style={{ marginTop: '2%' }} className="text-center">
+          <thead>
+            <tr>
+              <th>Nome do Alerta</th>
+              <th>Valor</th>
+              <th>Operador Condicional</th>
+              <th>Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.alertas.map(alerta => (
+              <tr key={alerta.id}>
+                <td>{alerta.Nome_Tipo_Alerta}</td>
+                <td>{alerta.Valor}</td>
+                <td>{alerta.Operador_Condicional}</td>
+                <td>
+                  <Button variant="primary" className="mr-2">
+                    <FontAwesomeIcon icon={faEdit} />
+                  </Button>
+                  <Button variant="danger">
+                    <FontAwesomeIcon icon={faTrash} />
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Container>
+    );
+  }
+}
