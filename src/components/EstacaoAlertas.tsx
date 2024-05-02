cfd14152d55e44565e6562977eed4906180b399b
Felipe Augusto Graciano <felipe98ju@hotmail.com>
2024-05-02 11:35:19 -0300

    fix: Alterado a forma de exibição dos alertas

--- src/components/EstacaoAlertas.tsx
+++ src/components/EstacaoAlertas.tsx
@@ -1,18 +1,21 @@
-import React, { useState, FormEvent } from "react";
+import React, { useState, FormEvent, useEffect } from "react";
 import { Container, Form, Button, Row, Col } from "react-bootstrap";
 import "../styles/TipoAlertasCadastro.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
-import { cadastrarAlerta } from "../services/apiService";
+import { cadastrarAlerta, listarAlertas } from "../services/apiService";
 
 interface State {
+  validated: boolean;
+  errorMessage: string | null;
+  tipoAlertas: TipoAlerta[];
+}
+
+interface TipoAlerta {
   Nome_Tipo_Alerta: string;
   Valor: string;
   Operador_Condicional: string;
   ID_Tipo_Alerta: number;
-  validated: boolean;
-  errorMessage: string | null;
-  dropdownOpen: boolean;
 }
 
 interface Props {
@@ -21,92 +24,36 @@ interface Props {
 
 const EstacaoAlertas: React.FC<Props> = ({ parametroId }) => {
   const [state, setState] = useState<State>({
-    Nome_Tipo_Alerta: "",
-    Valor: '0',
-    Operador_Condicional: "=",
-    ID_Tipo_Alerta: 0,
     validated: false,
     errorMessage: null,
-    dropdownOpen: false, // Inicialmente, o dropdown está fechado
+    tipoAlertas: []
   });
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  useEffect(() => {
+    const fetchTipoAlertas = async () => {
+      try {
+        const response = await listarAlertas();
+        const tipoAlertas = response.data
 
-    const form = e.currentTarget;
-
-    setState((prevState) => ({
-      ...prevState,
-      validated: true,
-    }));
-
-    const isAllFieldsFilled = Array.from(form.elements).every((element: any) => {
-      if (element.required && element.value.trim() === "") {
         setState((prevState) => ({
           ...prevState,
-          errorMessage: `Erro! Preencha o campo: ${element.name.replace("_", " ")}`,
+          tipoAlertas: tipoAlertas
         }));
-        return false;
+      } catch (error) {
+        console.error("Erro ao buscar tipos de alertas:", error);
       }
-      return true;
-    });
+    };
 
-    if (isAllFieldsFilled) {
-      const data = {
-        Nome_Tipo_Alerta: state.Nome_Tipo_Alerta,
-        Valor: state.Valor,
-        Operador_Condicional: state.Operador_Condicional,
-        ID_Tipo_Alerta: state.ID_Tipo_Alerta,
-      };
+    fetchTipoAlertas();
+  }, []);
 
-      console.log(data);
 
-      try {
-        const response = await cadastrarAlerta(data);
-        console.log("Response: " + JSON.stringify(response));
-        if (response.status === 201) {
-          setState((prevState) => ({
-            ...prevState,
-            errorMessage: "Tipo de alerta adicionado com sucesso!",
-          }));
-        }
-      } catch (error: any) {
-        console.error("Erro ao enviar informações para o backend:", error);
-        if (error.response.status === 400) {
-          setState((prevState) => ({
-            ...prevState,
-            errorMessage: "Erro: Dados inválidos!!",
-          }));
-        } else {
-          setState((prevState) => ({
-            ...prevState,
-            errorMessage: "Erro na requisição. Por favor, tente novamente mais tarde.",
-          }));
-        }
-      }
-    }
-  };
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    console.log(`Valor selecionado para ${name}: ${value}`);
-    setState((prevState) => ({
-      ...prevState,
-      [name]: value !== undefined ? value : "",
-    }));
-  };
-
-  const toggleDropdown = () => {
-    setState((prevState) => ({
-      ...prevState,
-      dropdownOpen: !prevState.dropdownOpen, // Alterna entre aberto e fechado
-    }));
-  };
 
   return (
     <Container className="tipoalerta">
       <h1 className="text-center">Selecionar Alertas</h1>
-      <Form className="mt-5" onSubmit={handleSubmit} noValidate validated={state.validated}>
+      {/* {parametroId} */}
+      <Form className="mt-5" noValidate validated={state.validated}>
         {state.errorMessage && (
           <div
             className={`alert ${state.errorMessage.includes("Erro") ? "alert-danger" : "alert-success"}`}
@@ -118,26 +65,31 @@ const EstacaoAlertas: React.FC<Props> = ({ parametroId }) => {
         <Row>
           <Col>
             <Form.Group>
-              <div className="search-bar">
-                <Form.Label className="search-label">Tipo Alerta:</Form.Label>
-                <div className="search-input-wrapper">
-                  <Form.Control
-                    as="select"
-                    name="tipo_alerta"
-                    onChange={handleChange}
-                    className="search-input"
-                    onClick={toggleDropdown} // Adiciona o manipulador de evento para alternar o estado do dropdown
-                  >
-                    <option>Teste 1</option>
-                    <option>Teste 2</option>
-                    <option>Teste 3</option>
-                    <option>Teste 4</option>
-                    <option>Teste 5</option>
-                    <option>Teste 6</option>
-                  </Form.Control>
-                  <FontAwesomeIcon icon={faAngleDown} className={`search-icon ${state.dropdownOpen ? 'open' : ''}`} onClick={toggleDropdown} /> {/* Adiciona o manipulador de evento para alternar o estado do dropdown */}
-                </div>
-              </div>
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>Selecionar</th>
+                    <th>ID</th>
+                    <th>Nome do Tipo de Alerta</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {state.tipoAlertas.map((tipoAlerta) => (
+                    <tr key={tipoAlerta.ID_Tipo_Alerta}>
+                      <td>
+                        <Form.Check
+                          type="checkbox"
+                          id={`checkbox_${tipoAlerta.ID_Tipo_Alerta}`}
+                          label=""
+                          
+                        />
+                      </td>
+                      <td>{tipoAlerta.ID_Tipo_Alerta}</td>
+                      <td>{tipoAlerta.Nome_Tipo_Alerta}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
             </Form.Group>
           </Col>
         </Row>
